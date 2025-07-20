1c5a22386b93419b1f352d11ae31ec77a6e0d708
ryanblyth <ryanjblyth@gmail.com>
2025-07-20 11:54:12 -0600

    add section - Text Over Image | Slide | From Bottom | Scrub

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ document.addEventListener("DOMContentLoaded", (event) =>{
     // DESKTOP ONLY
     "(min-width: 768px)": function () {
       // Shrink the left column
-      gsap.to(".section-start-onscreen", {
+      gsap.to(".section-slide-from-right-duration-start-onscreen", {
         flexBasis: "66%",
         duration: 2,
         ease: "power2.out",
+        onComplete: () => ScrollTrigger.refresh(),
         scrollTrigger: {
           trigger: ".section.section-slide-from-right-duration",
           start: "top 75%",
@@ -20,7 +21,7 @@ document.addEventListener("DOMContentLoaded", (event) =>{
       });
 
       // Expand and fade in the right column
-      gsap.to(".section-start-offscreen-right", {
+      gsap.to(".section-slide-from-right-duration-start-offscreen-right", {
         flexBasis: "33%",
         opacity: 1,
         duration: 2,
@@ -50,27 +51,43 @@ document.addEventListener("DOMContentLoaded", (event) =>{
     // MOBILE ONLY
     "(max-width: 767px)": function () {
       // Initially hide the image container on mobile
-      gsap.set(".section-start-offscreen-right", {
-        height: 0,
-        // y: -100
+      gsap.set(".section-slide-from-right-duration-start-offscreen-right", {
+        height: 0
       });
 
       // Slide the image down from above when triggered
-      gsap.to(".section-start-offscreen-right", {
+      gsap.to(".section-slide-from-right-duration-start-offscreen-right", {
         height: "auto",
-        // y: 0,
         ease: "none",
+        onComplete: () => ScrollTrigger.refresh(),
         scrollTrigger: {
           trigger: ".animate-slide-from-right-duration",
           start: "top 65%",
-          // end: "150% 100%",
-          // scrub: true,
           duration: 2,
-          markers: true
+          // markers: true
         }
       });
     }
   });
   /* End Section - Image | Slide | From Right | Duration */
+
+  /* Begin Section - Text Over Image | Slide | From Bottom | Scrub */
+  gsap.fromTo(
+    ".scroll-overlay",
+    { y: "150vh" },             // from
+    {
+      y: "33vh",                // to
+      scrollTrigger: {
+        trigger: ".image-pin-section",
+        start: "top top",
+        end: "+=100%",
+        scrub: true,
+        pin: true,
+        anticipatePin: 1,
+        markers: true
+      }
+    }
+  );
+  /* Begin Section - Text Over Image | Slide | From Bottom | Scrub */
 
 })
