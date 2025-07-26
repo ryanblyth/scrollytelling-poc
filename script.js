787a9a407ea308cbced7ee7d832d81baaac2e0a8
ryanblyth <ryanjblyth@gmail.com>
2025-07-26 08:45:55 -0600

    fix: update js selectors that changed in the HTML structure.

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", (event) =>{
         ease: "power2.out",
         onComplete: () => ScrollTrigger.refresh(),
         scrollTrigger: {
-          trigger: ".section.section-slide-from-right-duration",
+          trigger: ".section-slide-from-right-duration",
           start: "top 75%",
           toggleActions: "play reverse play reverse",
           // markers: true
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", (event) =>{
         duration: 2,
         ease: "power2.out",
         scrollTrigger: {
-          trigger: ".section.section-slide-from-right-duration",
+          trigger: ".section-slide-from-right-duration",
           start: "top 75%",
           toggleActions: "play reverse play reverse",
           // markers: true
